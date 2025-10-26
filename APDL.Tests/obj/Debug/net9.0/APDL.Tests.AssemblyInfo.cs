f2a8bb408770c7c3880f3cd31f2f05e1c26dbdf0
DannyGomes <1130552@isep.ipp.pt>
2025-10-26 15:57:27 +0000

    [US228]-Terminados testes

--- APDL.Tests/obj/Debug/net9.0/APDL.Tests.AssemblyInfo.cs
+++ APDL.Tests/obj/Debug/net9.0/APDL.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APDL.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22d0905a6598bf391e5452226753fe361272a8e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+656a70822e982a6ae6c4fd00626c51ef9e3936f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("APDL.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APDL.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
