3d98433116b1ca10a6ccf3b1145a06d7bb1ad062
DannyGomes <1130552@isep.ipp.pt>
2025-11-21 04:40:25 +0000

    Adicionado ip da vm a configuracao cors

--- APDL.SPA/src/environments/environment.prod.ts
+++ APDL.SPA/src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://10.9.11.75/api',
+  apiUrl: 'https://10.9.11.75/api',
   auth0: {
     domain: 'apdl-operations.eu.auth0.com',
     clientId: '9moFd6KqbCZnZImZrmhwHW3CTq6k9Qn0',
     authorizationParams: {
-      audience: 'http://10.9.11.75/api',
-      redirect_uri: window.location.origin
+      audience: 'https://localhost:5001/api',
+      redirect_uri: window.location.origin,
+      scope: 'openid profile email offline_access'
     }
   }
 };
--- APDL.SPA/src/environments/environment.ts
+++ APDL.SPA/src/environments/environment.ts
@@ -6,7 +6,8 @@ export const environment = {
     clientId: '9moFd6KqbCZnZImZrmhwHW3CTq6k9Qn0',
     authorizationParams: {
       audience: 'https://localhost:5001/api',
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
+      scope: 'openid profile email offline_access'
     }
   }
 };
